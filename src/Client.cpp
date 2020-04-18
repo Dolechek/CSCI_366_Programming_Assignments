130df0d126e58c4a6adced3d88f62dd7f503a10e
Dolechek <logand.msu@gmail.com>
2020-04-18 14:50:00 -0600

    whatever

--- src/Client.cpp
+++ src/Client.cpp
@@ -22,10 +22,31 @@ Client::~Client() {
 
 
 void Client::initialize(unsigned int player, unsigned int board_size){
+    Client::player = player;// this allows me to access 'player' in lower functions
+
+    vector<vector<int>> iBoard; // creating 2d vector of ints
+    iBoard.resize(board_size, vector<int>(board_size, 0)); // filling vector w/ 0's
+
+    string whoIsP = std::to_string(player); // whoIsP will change values depending on player 1 or 2
+    string fname = "player_" + whoIsP + ".action_board.json"; // fname holds whichever player_#.action_board.json is
+
+    ofstream array_ofp(fname); // create an output file stream
+    cereal::JSONOutputArchive write_archive(array_ofp); // initialize an archive on the file
+    write_archive(cereal::make_nvp("board", iBoard)); // serialize the data giving it a name
+
+    initialized = true; // I don't believe this is needed just yet. but here either way.
 }
 
 
 void Client::fire(unsigned int x, unsigned int y) {
+    string whoIsP = std::to_string(player); // whoIsP will change values depending on player 1 or 2
+    string fname = "player_" + whoIsP +".shot.json"; // fname holds whichever player_#.action_board.json is
+
+    ofstream ofs(fname); // create an output file stream
+    cereal::JSONOutputArchive opArchive(ofs); // initialize an archive on the file
+    /*https://stackoverflow.com/questions/47297648/using-cereal-to-deserialize-a-json-string
+     * found the CEREAL_NVP here. */
+    opArchive(CEREAL_NVP(x), CEREAL_NVP(y)); // serialize the data
 }
 
 
--- src/Server.cpp
+++ src/Server.cpp
@@ -26,12 +26,39 @@
  * @return length of the file in bytes
  */
 int get_file_length(ifstream *file){
+    file->seekg(0, ios::beg);
+    int start = file->tellg();
+    file->seekg(0, ios::end);
+    int end = file->tellg();
+    return end - start;
 }
 
 
 void Server::initialize(unsigned int board_size,
                         string p1_setup_board,
                         string p2_setup_board){
+    this->board_size = board_size;
+
+    /*Variables for bringing in the file*/
+    ifstream ifs1;
+    ifstream ifs2;
+
+    /*Open the files*/
+    ifs1.open(p1_setup_board);
+    if(ifs1.fail()){
+        throw ServerException("Error: Couldn't open" + p1_setup_board);
+    }
+    ifs2.open(p2_setup_board);
+    if(ifs2.fail()){
+        throw ServerException("Error: Couldn't open" + p2_setup_board);
+    }
+    if(get_file_length(&ifs1) != (board_size*(board_size+1))){
+        throw ServerException("Error: Incorrect Board Size");
+    }
+
+    if(get_file_length(&ifs2) != (board_size*(board_size+1))){
+        throw ServerException("Error: Incorrect Board Size");
+    }
 }
 
 
@@ -43,9 +70,87 @@ BitArray2D *Server::scan_setup_board(string setup_board_name){
 }
 
 int Server::evaluate_shot(unsigned int player, unsigned int x, unsigned int y) {
+    /*Local Variables*/
+    ifstream ifs;
+    string line;
+    vector<vector<char>> cBoard; // 2D vector of characters
+    /*two if statements to check and make sure there is only 2 players*/
+    if(player > 2 || player < 1){
+        throw ServerException("Error: Invalid player number.");
+    }
+    /*make sure we aren't out of bounds*/
+    if(y >= board_size || x >= board_size){
+        return OUT_OF_BOUNDS;
+    }
+
+    /*checking for hits and misses*/
+    /*Found the information for the 2D vector from this stack overflow link. Code used was the while loop
+     * and modified it a bit to work with what I wanted it to do.
+     * https://stackoverflow.com/questions/56554212/how-do-i-read-a-text-file-into-a-2d-vector
+     * */
+    if(player == 2){
+        ifs.open("player_1.setup_board.txt");
+    }else if(player == 1){
+        ifs.open("player_2.setup_board.txt");
+    }
+    while (std::getline(ifs, line)) {
+        std::vector<char> row;
+        for (char &c : line) {
+            if (c != '\n') {
+                row.push_back(c);
+            }
+        }
+        cBoard.push_back(row);
+    }
+    ifs.close(); // close the input file stream
+
+    /*logic to register hits or misses*/
+    if(cBoard[y][x] != '_')
+        return HIT;
+    else
+        return MISS;
 }
 
 
 int Server::process_shot(unsigned int player) {
-   return NO_SHOT_FILE;
+    /*code to open file and read in the coordinates provided by player*/
+    if(player > 2 || player < 1){
+        throw ServerException("Error: invalid player number");
+    }
+
+    string whoIsP = std::to_string(player); //whoIsP will be overridden by whoever just shot.
+    string input_file = "player_" + whoIsP + ".shot.json"; // string will concatenate for file name set to input_file
+    int x = 0,y = 0; // empty x and y variables
+    ifstream ifs(input_file); // open input file stream
+
+    /*checking to see if a shot has even been made*/
+    if(!ifs){
+        return NO_SHOT_FILE;
+    }
+
+    cereal::JSONInputArchive ipArchive(ifs); // initialize an archive on the file
+    ipArchive(x,y); // deserialize
+    ifs.close(); // close the input file stream
+
+    /*code to write back to the JSON FILE*/
+    int result = evaluate_shot(player,x,y);
+    string result_file = "player_" + whoIsP + ".result.json";
+    remove(result_file.c_str()); // remove serialization of file
+
+    /* Logic to handle if player 1 or 2 was the one that shot.
+     * since it wouldn't let me use whoIsP */
+    if(player == 1){
+        ofstream ofs(result_file);
+        cereal::JSONOutputArchive opArchive(ofs);
+        opArchive(CEREAL_NVP(result));
+        remove("player_1.shot.json");
+        return SHOT_FILE_PROCESSED;
+    }else if(player == 2){
+        ofstream ofs(result_file);
+        cereal::JSONOutputArchive opArchive(ofs);
+        opArchive(CEREAL_NVP(result));
+        remove("player_2.shot.json");
+        return SHOT_FILE_PROCESSED;
+    }
+
 }
